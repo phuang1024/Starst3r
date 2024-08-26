2d9b69d42bdf6181649b2bad3d08dd8224185945
Patrick Huang <huangpatrick16777216@gmail.com>
2024-08-26 09:38:39 -0500

    Fixed blender build.

--- build.sh
+++ build.sh
@@ -1,9 +1,33 @@
-cd build
+set -e
 
-cp -r ../starster .
-rm starster/dust3r starster/mast3r
+function build_python() {
+    mkdir -p build
+    cd build
 
-cp -r ../mast3r/mast3r starster/
-cp -r ../mast3r/dust3r/dust3r starster/
+    cp -r ../starster .
+    rm starster/dust3r starster/mast3r
 
-python setup.py sdist bdist_wheel
+    cp -r ../mast3r/mast3r starster/
+    cp -r ../mast3r/dust3r/dust3r starster/
+    rm starster/dust3r/dust3r
+    cp -r ../mast3r/dust3r/dust3r starster/dust3r/
+
+    python setup.py sdist bdist_wheel
+}
+
+function build_blender() {
+    mkdir -p build/blender
+    cd build/blender
+
+    cp -r ../../blender starster_blender
+    zip -r starster.zip starster_blender
+}
+
+if [ "$1" == "python" ]; then
+    build_python
+elif [ "$1" == "blender" ]; then
+    build_blender
+else
+    build_python
+    build_blender
+fi
