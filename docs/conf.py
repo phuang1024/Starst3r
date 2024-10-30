a9e905389e5e3d803853124e39d49b1e99174784
Patrick Huang <huangpatrick16777216@gmail.com>
2024-10-30 09:06:24 -0500

    Bump version.

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Starst3r'
 copyright = '2024, Patrick Huang'
 author = 'Patrick Huang'
-release = '0.4.0'
+release = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- starster/__init__.py
+++ starster/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from mast3r.model import AsymmetricMASt3R as Mast3rModel
 
